e96f287be84cfe2e8860b0f141289dee054dac5c
buptyuxin <buptyuxin@163.com>
2014-10-14 17:58:35 +0800

    .

--- study-core/src/main/java/com/ali/yanmo/study/job/JobManager.java
+++ study-core/src/main/java/com/ali/yanmo/study/job/JobManager.java
@@ -1,13 +1,11 @@
 package com.ali.yanmo.study.job;
 
-import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class JobManager {
 
-    private List<Job> jobs;
     private ThreadPoolExecutor threadPoolExecutor;
     private BlockingQueue<Runnable> jobQueue;
 
