14355b10ddb7c0a57f1336bf246ede8f55335758
buptyuxin <buptyuxin@163.com>
2014-10-15 17:10:05 +0800

    .

--- study-core/src/main/java/com/ali/yanmo/study/core/CoreTest.java
+++ study-core/src/main/java/com/ali/yanmo/study/core/CoreTest.java
@@ -1,9 +1,17 @@
 package com.ali.yanmo.study.core;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.MutablePropertyValues;
@@ -48,6 +56,18 @@ public class CoreTest {
         InputStream inputStream = CoreTest.class.getClassLoader().getResourceAsStream(filePath);
         parser.parse(inputStream);
         
+        //test HttpClient
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("http://www.baidu.com");
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            System.out.println(EntityUtils.toString(httpResponse.getEntity()));
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         beanFactory = new DefaultListableBeanFactory();
         GenericBeanDefinition bd = new GenericBeanDefinition();
         
